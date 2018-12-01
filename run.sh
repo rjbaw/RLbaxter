d831c15793a4464a460278f0b954586bdbf5627e
ezvk7740 <ezvk7740@github.com>
2018-12-01 05:30:09 +0000

    fix bash for overall

--- run.sh
+++ run.sh
@@ -2,7 +2,18 @@
 xhost +local:
 /usr/local/bin/./txdocker run -it --net=host \
   --user=$(id -u) \
-  -e DISPLAY=$DISPLAY \
+  --device /dev/nvhost-as-gpu \
+  --device /dev/nvhost-ctrl \
+  --device /dev/nvhost-ctrl-gpu \
+  --device /dev/nvhost-ctxsw-gpu \
+  --device /dev/nvhost-dbg-gpu \
+  --device /dev/nvhost-gpu \
+  --device /dev/nvhost-prof-gpu \
+  --device /dev/nvhost-sched-gpu \
+  --device /dev/nvhost-tsg-gpu \
+  --device /dev/nvmap \
+  --device /dev/snd \
+  -e DISPLAY \
   -e QT_GRAPHICSSYSTEM=native \
   -e CONTAINER_NAME=ros-kinetic-dev \
   -e USER=$USER \
