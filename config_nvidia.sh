1ddf352dc3ef47b6397cb2b6340c0a66501c9345
ezvk7740 <ezvk7740@github.com>
2018-12-07 15:52:00 +0000

    checking update

--- config_nvidia.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-add-apt-repository ppa:xorg-edgers/ppa -y
-apt-get update
-
-apt-get install nvidia-355
--- envdocker.sh
+++
@@ -1,7 +0,0 @@
-mkvirtualenv ros -p python3
-workon ros
-
-# rl toolbox
-git clone https://github.com/araffin/robotics-rl-srl --recursive
-apt-get install swig
-
