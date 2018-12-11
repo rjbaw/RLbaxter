ded2e5d464d6b6b8703e3cbf697e96f69ab67595
ezvk7740 <ezvk7740@github.com>
2018-12-11 19:21:00 +0000

    usb config file

--- usb.sh
+++ usb.sh
@@ -5,11 +5,13 @@ sudo umount /mnt/sdb
 sudo umount /mnt/sda
 sudo mount -t ext4 -o rw /dev/sda /mnt/sdb
 sudo mount -t ext4 -o rw /dev/sdb1 /mnt/sda
-sudo chown -R nvidia /mnt/sdb
+#sudo chown -R nvidia /mnt/sdb
 sudo chown -R nvidia /mnt/sda
 
 #sudo ln -s /mnt/sdb ~/usb
-
+#sudo ln -s /mnt/sda ~/usb1
 # sudo umount /mnt/sdb
 
 #ln -s /mnt/sdb/RLbaxter ~/RLbaxter
+sudo systemctl daemon-reload
+sudo systemctl start docker
